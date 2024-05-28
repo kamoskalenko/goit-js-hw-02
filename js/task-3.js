9973e06f6df13d9620ff08f21efdc5afa50fb58e
kmositua <kmositua@gmail.com>
2024-05-28 12:38:53 +0100

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-// "use script";
+//"use script";
 function checkForSpam(message) {
     if (message.toLowerCase().includes("spam")) {
         return true;
@@ -11,11 +11,15 @@ function checkForSpam(message) {
     }
 }
 
-// console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-// console.log(checkForSpam("Trust me, this is not a spam message")); // true
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
+// function checkForSpam(message) {
+//     const normalizedMessage = message.toLowerCase()
+//     return normalizedMessage.includes('spam') ||  normalizedMessage.includes('sale')
+// }
